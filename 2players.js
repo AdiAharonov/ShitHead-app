cb9925c552404c2ff072c4afc07a7d76f3cbe68d
AdiAharonov <59651949+AdiAharonov@users.noreply.github.com>
2020-02-25 23:58:38 +0200

    25/02/2020 Update
    Game starting initialized

--- 2players.js
+++ 2players.js
@@ -1,3 +1,9 @@
+//Begin game
+
+
+
+
+
 let deck = [ 'AC', 'AD', 'AH', 'AS',
 '2C', '2D', '2H', '2S',
 '3C', '3D', '3H', '3S', 
@@ -11,6 +17,8 @@ let deck = [ 'AC', 'AD', 'AH', 'AS',
 'JC', 'JD', 'JH', 'JS',
 'QC', 'QD', 'QH', 'QS', 
 'KC', 'KD', 'KH', 'KS'];
+
+
 
 let hiddenP1, hiddenP2, hiddenP3, hiddenE1, hiddenE2, hiddenE3,
 handCard1, handCard2, handCard3, handCard4, handCard5, handCard6,
@@ -40,7 +48,11 @@ eCard6 = document.getElementById('eCard6');
 
 let currentIndex;
 let currentCard;
+let start = document.getElementById('start');
 
+
+
+// Players side
 
 function selectCard(card) {
 
@@ -71,7 +83,7 @@ function selectCard(card) {
     } 
  
 } 
-
+// To pick initial cards over hidden cards
 function pickPlace() {
     let fixedCard = currentCard;
     
@@ -79,48 +91,55 @@ function pickPlace() {
     
     fixedCard.classList.remove('pause');
     fixedCard.classList.add('moveCard');
-    fixedCard.classList.remove('hover');
-    
-    playerRevealedCards = playerHand.splice(currentIndex, 1);
+    $(fixedCard).attr( "onclick", "goBack(this)");
     $(".moveCard").appendTo("#playerRevealedCards");
-    $(fixedCard).prop( "onclick", null );
+
+    
+    // $(fixedCard).prop( "onclick", null );
+    
+
     if (playerRevealedCards.length === 2) {
         return;
     }
 
     
-
-    // var xPosition = clickPlace.clientX;
-    // var yPosition = clickPlace.clientY;
-
-    // var translate3dValue = "translate3d(" + xPosition + "px," + yPosition + "px, 0)";
-    // fixedCard.style.transform = translate3dValue;
-    
-    
     currentIndex = null;
     currentCard = null;
     holdingCard = false;
      
-    
 
 
 }
 
+function goBack(card) {
 
+    // card.classList.add('hover');
+    $(card).appendTo("#playersHandCards");
+    card.classList.remove('moveCard');
+    
+    $(card).appendTo("#playerHand");
+    $(card).attr( "onclick", " selectCard(this)");
+    
+}
 
-// function updateArray(array) {
+function ready() {
 
-//     for (i = 0; i <= array.length; i++) {
-//         console.log(array[i]);
-//         // attachPicToCard(array[i]);
+    for (const card of playerHand) {
+        
+        if (card.id === 'fixedCard') {
 
-//     }
+            card.setAttribute('onClick', null);
+            card.classList.remove('hover');
+            let moveCard = playerHand.splice(playerHand.indexOf(card), 1);
+            playerRevealedCards.push(moveCard[0]);
+            
 
-// }
+        }
 
+    }
 
-
-//start game
+    
+}
 
 
 //all the needed arrays
@@ -132,15 +151,41 @@ let cardsPlace = document.getElementById('cardsPlace');
 cardsPlace = [];
 let playerRevealedCards = document.getElementById('playerRevealedCards');
 playerRevealedCards = [];
+let enemyRevealedCards = document.getElementById('enemyRevealedCards');
+enemyRevealedCards = [];
 let holdingCard = false;
 let gameStarted;
 
 function startGame() {
+
+    
+
     setHandCards(playerHiddenCards, playerHand);
     setEnemyCards(enemyHiddenCards, enemyHand);
-    gameStarted = true;
+    $(start).attr("onClick", "replay()"); 
+    start.innerHTML = "Replay";
+         
+    // Ready button
+
+    let ready = document.createElement("button");
+    ready.innerHTML = "Ready";
+    ready.classList.add('ready');
+    document.body.appendChild(ready);
+    ready.setAttribute("onClick", "ready()");
+
     
+    enemyHandStart()
+
+    gameStarted = true;
+
+       
 }
+
+ function replay() {
+    
+    location.reload();
+
+ }
 
 
 function setHandCards(hiddenarray, array) {
@@ -148,7 +193,7 @@ function setHandCards(hiddenarray, array) {
     
     for (let card of array) {
         const rndCard = randomCard();
-        card.setAttribute("value", rndCard);
+        card.setAttribute("name", rndCard);
         card.classList.remove('card');
         card.classList.add('hover');
         card.innerHTML="<img class='cardflipped' src='css/cards/" + rndCard + ".jpg'/>";
@@ -157,7 +202,7 @@ function setHandCards(hiddenarray, array) {
     
    for (let card of hiddenarray) {
     const rndCard = randomCard();
-    card.setAttribute("value", rndCard);
+    card.setAttribute("name", rndCard);
     
    }
 
@@ -169,7 +214,7 @@ function setEnemyCards(hiddenarray, array) {
 
     for (let card of array) {
         const rndCard = randomCard();
-        card.setAttribute("value", rndCard);
+        card.setAttribute("name", rndCard);
         
         
          
@@ -178,7 +223,7 @@ function setEnemyCards(hiddenarray, array) {
     
    for (let card of hiddenarray) {
     const rndCard = randomCard();
-    card.setAttribute("value", rndCard);
+    card.setAttribute("name", rndCard);
     
    }
 
@@ -193,3 +238,147 @@ function randomCard() {
 }
  
 
+
+
+//enemy side
+
+function enemyHandStart() {
+    
+    for (const card of enemyHand) {
+        convertNameToValue(card)
+        console.log(card.getAttribute('name'));
+    }
+
+
+    checkForValue('10')
+    checkForValue('3')
+    checkForValue('2')
+    checkForValue('1')
+    checkForValue('13')
+    checkForValue('12')
+    checkForValue('11')
+    checkForValue('9')
+    checkForValue('8')
+    checkForValue('7')
+    checkForValue('6')
+    checkForValue('5')
+     
+}
+
+function setInitialEnemyCards(card) {
+    currentIndex = enemyHand.indexOf(card);
+    
+    card.classList.add("moveECard");
+    card.setAttribute("id", "fixedECard");
+
+    currentCard = document.getElementById('fixedECard');
+    
+
+    $('.moveECard').appendTo("#enemyRevealedCards");
+    card.innerHTML="<img class='cardflipped' src='css/cards/" + card.getAttribute("name") + ".jpg'/>";
+    card.classList.remove("card");
+
+
+    currentCard = enemyHand.splice(currentIndex, 1);
+    enemyRevealedCards.push(currentCard[0]);
+
+    currentCard = null;
+    currentIndex = null;
+    
+//    console.log(card.getAttribute('name'), enemyRevealedCards);
+}
+
+
+function convertNameToValue(card) {
+    
+    if (card.getAttribute('name') === 'AC' || card.getAttribute('name') === 'AD' || card.getAttribute('name') === 'AH' || card.getAttribute('name') === 'AS' ) {
+
+        card.setAttribute('value', '1');
+
+    }
+    if (card.getAttribute('name') === '2C' || card.getAttribute('name') === '2D' || card.getAttribute('name') === '2H' || card.getAttribute('name') === '2S' ) {
+
+        card.setAttribute('value', '2');
+
+    }
+    if (card.getAttribute('name') === '3C' || card.getAttribute('name') === '3D' || card.getAttribute('name') === '3H' || card.getAttribute('name') === '3S' ) {
+
+        card.setAttribute('value', '3');;
+
+    }
+    if (card.getAttribute('name') === '4C' || card.getAttribute('name') === '4D' || card.getAttribute('name') === '4H' || card.getAttribute('name') === '4S' ) {
+
+        card.setAttribute('value', '4');
+
+    }
+    if (card.getAttribute('name') === '5C' || card.getAttribute('name') === '5D' || card.getAttribute('name') === '5H' || card.getAttribute('name') === '5S' ) {
+
+        card.setAttribute('value', '5');
+
+    }
+    if (card.getAttribute('name') === '6C' || card.getAttribute('name') === '6D' || card.getAttribute('name') === '6H' || card.getAttribute('name') === '6S' ) {
+
+        card.setAttribute('value', '6');
+
+    }
+    if (card.getAttribute('name') === '7C' || card.getAttribute('name') === '7D' || card.getAttribute('name') === '7H' || card.getAttribute('name') === '7S' ) {
+
+        card.setAttribute('value', '7');
+
+    }
+    if (card.getAttribute('name') === '8C' || card.getAttribute('name') === '8D' || card.getAttribute('name') === '8H' || card.getAttribute('name') === '8S' ) {
+
+        card.setAttribute('value', '8');
+
+    }
+    if (card.getAttribute('name') === '9C' || card.getAttribute('name') === '9D' || card.getAttribute('name') === '9H' || card.getAttribute('name') === '9S' ) {
+
+        card.setAttribute('value', '9');
+
+    }
+    if (card.getAttribute('name') === '10C' || card.getAttribute('name') === '10D' || card.getAttribute('name') === '10H' || card.getAttribute('name') === '10S' ) {
+
+        card.setAttribute('value', '10');
+
+    }
+    if (card.getAttribute('name') === 'JC' || card.getAttribute('name') === 'JD' || card.getAttribute('name') === 'JH' || card.getAttribute('name') === 'JS' ) {
+
+        card.setAttribute('value', '11');
+
+    }
+    if (card.getAttribute('name') === 'QC' || card.getAttribute('name') === 'QD' || card.getAttribute('name') === 'QH' || card.getAttribute('name') === 'QS' ) {
+
+        card.setAttribute('value', '12');
+
+    }
+    if (card.getAttribute('name') === 'KC' || card.getAttribute('name') === 'KD' || card.getAttribute('name') === 'KH' || card.getAttribute('name') === 'KS' ) {
+
+        card.setAttribute('value', '13');
+
+
+    }
+    // console.log(card.getAttribute('value'), card.getAttribute('name'));
+    
+}
+
+function checkForValue(value) {
+
+for (const card of enemyHand) {
+
+    if (enemyRevealedCards.length === 3) {
+                
+        return;
+      
+    }
+
+    if(card.getAttribute('value') === value) {
+        setInitialEnemyCards(card)
+    }
+
+    
+
+}
+
+    
+    
+}
